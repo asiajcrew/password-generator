13c81eb5ed1041e3986d1683fa48ab77f20ce2e1
asiajcrew <asia_jcrew96@yahoo.com>
2021-12-02 08:06:32 -0500

    added window confirm for each character rule

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,11 @@ var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
 var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var specialCharacterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '~', '`', '?', ',', '.', ';', ':', '"', '|']
 
+var promptLength = window.prompt("How many characters would you like your password to be? (Please choose between 8 to 128 characters)")
+var lowerCaseChoice = window.confirm("Would you like to use lowercase leters in your password?")
+var upperCaseChoice = window.confirm("Would you like to use uppercase leters in your password?")
+var numberChoice = window.confirm("Would you like to use numbers in your password?")
+var specialCharacterChoice = window.confirm("Would you like to use special characters in your password?")
 
 // Write password to the #password input
 function writePassword() {
